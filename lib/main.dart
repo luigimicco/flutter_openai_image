22ac53493be1394f3d041b9b29ac3ee1d5b335e1
Luigi Micco <luigi.micco@gmail.com>
2022-12-25 21:58:54 +0100

    add http request

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import './openai_key.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +20,77 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: OpenAi(),
     );
+  }
+}
+
+class OpenAi extends StatefulWidget {
+  const OpenAi({super.key});
+
+  @override
+  State<OpenAi> createState() => _OpenAiState();
+}
+
+class _OpenAiState extends State<OpenAi> {
+  String result = "";
+  final textController = TextEditingController();
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Openai test"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: textController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "put description here",
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  getData(textController.value.toString()).then((value) {
+                    result = json.decode(value.body)['data'][0]['url'];
+                    print(result);
+                    setState(() {});
+                  });
+                },
+                child: const Text("Genera")),
+            const Spacer(),
+            SafeArea(child: Text(result))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<Response> getData(String description) async {
+    var endPoint = Uri.https('api.openai.com', '/v1/images/generations');
+
+    Map<String, String> headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $OPENAI_KEY'
+    };
+
+    final body = json.encode({
+      'prompt': description,
+      'n': 1,
+      'size': '512x512',
+    });
+
+    return http.post(endPoint, headers: headers, body: body);
   }
 }
